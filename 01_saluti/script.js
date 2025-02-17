5c3dfa1c0bc49f7d61cba45f2427133d614640ba
mattiapennisi <mattiapennisi0@gmail.com>
2025-02-17 15:18:41 +0100

    add script 01

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,9 +4,15 @@ const userName = 'Mario';
 
 // Dichiara la funzione qui.
 
+function greetings (str) {
+    const string = 'Ciao ' + str
+    return string
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+let result = greetings('Mattia')
 
+console.log(result)
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
